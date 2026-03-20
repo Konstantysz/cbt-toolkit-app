0a3038cf9dcacd083dbaafd14ac14b5501d80a2c
konstantysz <kostekszumigaj@gmail.com>
2026-03-20 23:18:57 +0100

    feat: RecordListScreen with list, FAB, empty state

--- src/app/(tools)/thought-record/[id].tsx
+++ src/app/(tools)/thought-record/[id].tsx
@@ -1,4 +1,4 @@
 import React from 'react';
-import { RecordDetailScreen } from '../../../../tools/thought-record/screens/RecordDetailScreen';
+import { RecordDetailScreen } from '../../../tools/thought-record/screens/RecordDetailScreen';
 
 export default RecordDetailScreen;
--- src/app/(tools)/thought-record/new.tsx
+++ src/app/(tools)/thought-record/new.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
-import { NewRecordFlow } from '../../../../tools/thought-record/screens/NewRecordFlow';
+import { NewRecordFlow } from '../../../tools/thought-record/screens/NewRecordFlow';
 
 export default NewRecordFlow;
